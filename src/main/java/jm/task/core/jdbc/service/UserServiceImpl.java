671d8667f5a927a2de506f04639f0b0ccc1a8cdb
SergeyEmalyanov <101347695+SergeyEmalyanov@users.noreply.github.com>
2023-05-12 06:25:39 +0300

    Update UserServiceImpl.java
    Сделал через интерфейс.

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 public class UserServiceImpl implements UserService {
 
-    private final UserDaoJDBCImpl userDaoJDBC = new UserDaoJDBCImpl();
+    private final UserDao userDaoJDBC = new UserDaoJDBCImpl();
 
     public void createUsersTable() {
         userDaoJDBC.createUsersTable();
